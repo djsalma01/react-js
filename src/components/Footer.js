c04e26fa49e526a7715eb5d3cbb4de799a8287c7
djsalma01 <sdjadi@outlook.com>
2021-03-29 16:01:48 +0100

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./footer.css"
+
+function footer() {
+    return (
+        
+<footer class="footer mt-auto py-3 bg-light">
+  <div class="container row">
+  <div class="col-sm-3">
+      1 of 3
+    </div>
+    <div class="col-sm-6">
+    <span class="text-muted"><img src="../../public/Logo-sedia.png"></img></span>
+    </div>
+    <div class="col-sm-3">
+      <p>Contacts</p>
+    </div>
+  </div>
+    
+  
+</footer>
+    )
+}
+
+export default footer
